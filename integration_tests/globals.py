60d37b3fb51e4a6949fb03916c77bff7f439c8fd
Carlos <dose.lucky.sake@cloak.id>
2024-05-28 10:45:34 -0700

    - updating the globals

--- integration_tests/globals.py
+++ integration_tests/globals.py
@@ -19,31 +19,45 @@ SESS_V2_ARGS = {
     "remote_port": 11161,
     "community": "public",
 }
-SESS_V3_ARGS = {
+SESS_V3_MD5_ARGS = {
     "version": 3,
     "hostname": "localhost",
     "remote_port": 11161,
+    "auth_protocol": "MD5",
     "security_level": "authPriv",
     "security_username": "initial",
     "privacy_password": "priv_pass",
     "auth_password": "auth_pass",
 }
 
-SESS_TYPES = [SESS_V1_ARGS, SESS_V2_ARGS, SESS_V3_ARGS]
+SESS_V3_SHA_ARGS = {
+    "version": 3,
+    "hostname": "localhost",
+    "remote_port": 11161,
+    "auth_protocol": "SHA",
+    "security_level": "authPriv",
+    "security_username": "secondary",
+    "privacy_password": "priv_second",
+    "auth_password": "auth_second",
+}
+
+SESS_TYPES = [SESS_V1_ARGS, SESS_V2_ARGS, SESS_V3_MD5_ARGS, SESS_V3_SHA_ARGS]
+SESS_TYPES_NAMES = ["SESS_V1_ARGS", "SESS_V2_ARGS", "SESS_V3_MD5_ARGS", "SESS_V3_SHA_ARGS"]
 
 # Print SNMP information
-PRINT_SNMP_INFO = True
+PRINT_SNMP_INFO = False
 
 
 # Define a function that will be executed in a separate process or thread
 def worker(request_type: str):
     try:
-        sleep(uniform(0.0, 0.250))
-        sess_type = randint(0, 2)
+        # We sleep to not burden our SNMP Server
+        sleep(uniform(0.0, 0.500))
+        sess_type = randint(0, len(SESS_TYPES) - 1)
         sess = Session(**SESS_TYPES[sess_type])
 
         print(
-            f"\tWorker using: sess_type - {sess_type} with request_type - {request_type}"
+            f"\tWorker using: sess_type - {SESS_TYPES_NAMES[sess_type]} with request_type - {request_type}"
         )
 
         if request_type == "get":
