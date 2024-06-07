f9a1f43734ed6b8b37ae85a867a6c9d4a3c65d66
Carlos <dose.lucky.sake@cloak.id>
2024-06-07 12:21:07 -0700

    Feature/fix pypi deployment (#103)
    * Using Test Parameters
    
    - using tests/conf parameters instead of the ones of globals. One source of truth.
    
    * - attempt to fix pypi test GA.
    
    * - comment out check for changes for now.
    
    * - trying macos 13
    
    * - revert to latest. Fixing in toml.
    
    * - tryin an upgrade too
    
    * attempt to make folder
    
    * - remove dir too
    
    * - revert del of wheel folder
    
    * -make all for macos
    
    * - moving CIBW_ARCH = "all"
    
    * - no CIBW_ARCH = "all"
    
    * Test Globals
    
    - moving test globals to a file that doens't use pytest so that the integration tests can use them.
    
    * Update conftest.py
    
    - fix the local import.
    
    * Update globals.py
    
    - import the file we care about.
    
    * Update globals.py
    
    - revert the sys path doesn't work
    
    * Docs
    
    - updating docs with new examples
    - bumping line length to 100
    - adding py 3.12 support.
    
    * Sphinx Docs Update
    
    - updated the sphinx docs
    - prepping for PR. CI builds for MacOS are broken for now. Waiting on a fix in the image from upstream.
    
    * Update pyproject.toml
    
    - possible fix for macos GA builds

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,16 @@ import logging
 import pytest
 from sys import version_info
 from subprocess import Popen, DEVNULL
-
 import ezsnmp
+from .session_parameters import (
+    SESS_V1_ARGS,
+    SESS_V2_ARGS,
+    SESS_V3_MD5_DES_ARGS,
+    SESS_V3_MD5_AES_ARGS,
+    SESS_V3_SHA_AES_ARGS,
+    SESS_V3_SHA_NO_PRIV_ARGS,
+    SESS_V3_MD5_NO_PRIV_ARGS,
+)
 
 assert version_info[0] == 3 and version_info[1] >= 8
 
@@ -46,77 +54,6 @@ def snmp_set_via_cli(oid, value, type):
 # Disable logging for the C interface
 snmp_logger = logging.getLogger("ezsnmp.interface")
 snmp_logger.disabled = True
-
-
-SESS_V1_ARGS = {
-    "version": 1,
-    "hostname": "localhost",
-    "remote_port": 11161,
-    "community": "public",
-}
-
-SESS_V2_ARGS = {
-    "version": 2,
-    "hostname": "localhost",
-    "remote_port": 11161,
-    "community": "public",
-}
-
-SESS_V3_MD5_DES_ARGS = {
-    "version": 3,
-    "hostname": "localhost",
-    "remote_port": 11161,
-    "auth_protocol": "MD5",
-    "security_level": "authPriv",
-    "security_username": "initial_md5_des",
-    "privacy_protocol": "DES",
-    "privacy_password": "priv_pass",
-    "auth_password": "auth_pass",
-}
-
-SESS_V3_MD5_AES_ARGS = {
-    "version": 3,
-    "hostname": "localhost",
-    "remote_port": 11161,
-    "auth_protocol": "MD5",
-    "security_level": "authPriv",
-    "security_username": "initial_md5_aes",
-    "privacy_protocol": "AES",
-    "privacy_password": "priv_pass",
-    "auth_password": "auth_pass",
-}
-
-SESS_V3_SHA_AES_ARGS = {
-    "version": 3,
-    "hostname": "localhost",
-    "remote_port": 11161,
-    "auth_protocol": "SHA",
-    "security_level": "authPriv",
-    "security_username": "secondary_sha_aes",
-    "privacy_protocol": "AES",
-    "privacy_password": "priv_second",
-    "auth_password": "auth_second",
-}
-
-SESS_V3_SHA_NO_PRIV_ARGS = {
-    "version": 3,
-    "hostname": "localhost",
-    "remote_port": 11161,
-    "auth_protocol": "SHA",
-    "security_level": "authNoPriv",
-    "security_username": "secondary_sha_no_priv",
-    "auth_password": "auth_second",
-}
-
-SESS_V3_MD5_NO_PRIV_ARGS = {
-    "version": 3,
-    "hostname": "localhost",
-    "remote_port": 11161,
-    "auth_protocol": "MD5",
-    "security_level": "auth_without_privacy",
-    "security_username": "initial_md5_no_priv",
-    "auth_password": "auth_pass",
-}
 
 
 @pytest.fixture(
