715f8ee7a0d3f02f571ed191047902145eb58c4a
Carlos <dose.lucky.sake@cloak.id>
2024-06-06 19:49:03 -0700

    Update globals.py
    - revert the sys path doesn't work

--- integration_tests/globals.py
+++ integration_tests/globals.py
@@ -12,14 +12,76 @@ from threading import get_native_id
 import os
 import sys
 
-sys.path.insert(1, os.path.abspath("../tests/session_parameters.py"))
+SESS_V1_ARGS = {
+    "version": 1,
+    "hostname": "localhost",
+    "remote_port": 11161,
+    "community": "public",
+}
 
-from session_parameters import (
-    SESS_V1_ARGS,
-    SESS_V2_ARGS,
-    SESS_V3_MD5_DES_ARGS,
-    SESS_V3_SHA_AES_ARGS,
-)
+SESS_V2_ARGS = {
+    "version": 2,
+    "hostname": "localhost",
+    "remote_port": 11161,
+    "community": "public",
+}
+
+SESS_V3_MD5_DES_ARGS = {
+    "version": 3,
+    "hostname": "localhost",
+    "remote_port": 11161,
+    "auth_protocol": "MD5",
+    "security_level": "authPriv",
+    "security_username": "initial_md5_des",
+    "privacy_protocol": "DES",
+    "privacy_password": "priv_pass",
+    "auth_password": "auth_pass",
+}
+
+SESS_V3_MD5_AES_ARGS = {
+    "version": 3,
+    "hostname": "localhost",
+    "remote_port": 11161,
+    "auth_protocol": "MD5",
+    "security_level": "authPriv",
+    "security_username": "initial_md5_aes",
+    "privacy_protocol": "AES",
+    "privacy_password": "priv_pass",
+    "auth_password": "auth_pass",
+}
+
+SESS_V3_SHA_AES_ARGS = {
+    "version": 3,
+    "hostname": "localhost",
+    "remote_port": 11161,
+    "auth_protocol": "SHA",
+    "security_level": "authPriv",
+    "security_username": "secondary_sha_aes",
+    "privacy_protocol": "AES",
+    "privacy_password": "priv_second",
+    "auth_password": "auth_second",
+}
+
+SESS_V3_SHA_NO_PRIV_ARGS = {
+    "version": 3,
+    "hostname": "localhost",
+    "remote_port": 11161,
+    "auth_protocol": "SHA",
+    "security_level": "authNoPriv",
+    "security_username": "secondary_sha_no_priv",
+    "auth_password": "auth_second",
+}
+
+SESS_V3_MD5_NO_PRIV_ARGS = {
+    "version": 3,
+    "hostname": "localhost",
+    "remote_port": 11161,
+    "auth_protocol": "MD5",
+    "security_level": "auth_without_privacy",
+    "security_username": "initial_md5_no_priv",
+    "auth_password": "auth_pass",
+}
+
 
 SESS_TYPES = [SESS_V1_ARGS, SESS_V2_ARGS, SESS_V3_MD5_DES_ARGS, SESS_V3_SHA_AES_ARGS]
 SESS_TYPES_NAMES = [
