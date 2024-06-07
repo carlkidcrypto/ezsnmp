6a0b4d20d03085e2c123454c34dc99f360e8d780
Carlos <dose.lucky.sake@cloak.id>
2024-06-06 19:38:01 -0700

    Test Globals
    - moving test globals to a file that doens't use pytest so that the integration tests can use them.

--- integration_tests/globals.py
+++ integration_tests/globals.py
@@ -14,7 +14,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath("../tests/"))
 
-from conftest import (
+from session_parameters import (
     SESS_V1_ARGS,
     SESS_V2_ARGS,
     SESS_V3_MD5_DES_ARGS,
