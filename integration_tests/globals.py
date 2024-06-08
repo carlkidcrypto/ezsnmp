3cbe46ef611f154baa12a89dab06778fed4e6d0a
Carlos <dose.lucky.sake@cloak.id>
2024-06-07 20:57:31 -0700

    Update globals.py
    - current workaound to usm error is too just try again.

--- integration_tests/globals.py
+++ integration_tests/globals.py
@@ -9,9 +9,6 @@ from ezsnmp.exceptions import EzSNMPConnectionError, EzSNMPError
 from os import getpid
 from threading import get_native_id
 
-import os
-import sys
-
 SESS_V1_ARGS = {
     "version": 1,
     "hostname": "localhost",
@@ -97,56 +94,71 @@ PRINT_SNMP_INFO = False
 
 # Define a function that will be executed in a separate process or thread
 def worker(request_type: str):
-    try:
-        # We sleep to not burden our SNMP Server
-        sleep(uniform(0.0, 0.250))
-        sess_type = randint(0, len(SESS_TYPES) - 1)
-        sess = Session(**SESS_TYPES[sess_type])
+    are_we_done = False
+    connection_error_counter = 0
+    usm_unknown_security_name_counter = 0
+    while are_we_done != True:
+        try:
+            # We sleep to not burden our SNMP Server
+            sleep(uniform(0.0, 0.250))
+            sess_type = randint(0, len(SESS_TYPES) - 1)
+            sess = Session(**SESS_TYPES[sess_type])
 
-        print(
-            f"\tWorker using: sess_type - {SESS_TYPES_NAMES[sess_type]} with request_type - {request_type}. With PID: {getpid()} and TID: {get_native_id()}"
-        )
-
-        if request_type == "get":
-            test = sess.get(
-                [("sysUpTime", "0"), ("sysContact", "0"), ("sysLocation", "0")]
-            )
-
-            # Access the result to ensure that the data is actually retrieved
-            for item in test:
-                if PRINT_SNMP_INFO:
-                    print(f"\t{item.oid} - {item.value}")
-
-            del test
-
-        elif request_type == "walk":
-            test = sess.walk(".")
-
-            # Access the result to ensure that the data is actually retrieved
-            for item in test:
-                if PRINT_SNMP_INFO:
-                    print(f"\t{item.oid} - {item.value}")
-
-            del test
-
-        elif request_type == "bulkwalk":
-            test = sess.bulkwalk(".")
-
-            # Access the result to ensure that the data is actually retrieved
-            for item in test:
-                if PRINT_SNMP_INFO:
-                    print(f"\t{item.oid} - {item.value}")
-
-            del test
-
-    except EzSNMPConnectionError:
-        # We bombarded the SNMP server with too many requests...
-        print(
-            f"\tEzSNMPConnectionError: Connection to the SNMP server was lost. For a worker with PID: {getpid()} and TID: {get_native_id()}"
-        )
-
-    except EzSNMPError as e:
-        if str(e) == "USM unknown security name (no such user exists)":
             print(
-                f"\tEzSNMPError: {e}. For a worker with PID: {getpid()} and TID: {get_native_id()}"
+                f"\tWorker using: sess_type - {SESS_TYPES_NAMES[sess_type]} with request_type - {request_type}. With PID: {getpid()} and TID: {get_native_id()}"
             )
+
+            if request_type == "get":
+                test = sess.get(
+                    [("sysUpTime", "0"), ("sysContact", "0"), ("sysLocation", "0")]
+                )
+
+                # Access the result to ensure that the data is actually retrieved
+                for item in test:
+                    if PRINT_SNMP_INFO:
+                        print(f"\t{item.oid} - {item.value}")
+
+                del test
+
+            elif request_type == "walk":
+                test = sess.walk(".")
+
+                # Access the result to ensure that the data is actually retrieved
+                for item in test:
+                    if PRINT_SNMP_INFO:
+                        print(f"\t{item.oid} - {item.value}")
+
+                del test
+
+            elif request_type == "bulkwalk" and sess.version != 1:
+                test = sess.bulkwalk(".")
+
+                # Access the result to ensure that the data is actually retrieved
+                for item in test:
+                    if PRINT_SNMP_INFO:
+                        print(f"\t{item.oid} - {item.value}")
+
+                del test
+            
+            del sess
+            are_we_done = True
+            print(f"\tFor a worker with PID: {getpid()} and TID: {get_native_id()}")
+            print(f"\t\tconnection_error_counter: {connection_error_counter}")
+            print(f"\t\tusm_unknown_security_name_counter: {usm_unknown_security_name_counter}")
+
+        except EzSNMPConnectionError:
+            # We bombarded the SNMP server with too many requests...
+            # print(
+            #     f"\tEzSNMPConnectionError: Connection to the SNMP server was lost. For a worker with PID: {getpid()} and TID: {get_native_id()}"
+            # )
+            connection_error_counter += 1
+
+        except EzSNMPError as e:
+            if str(e) == "USM unknown security name (no such user exists)":
+                # print(
+                #     f"\tEzSNMPError: {e}. For a worker with PID: {getpid()} and TID: {get_native_id()}"
+                # )
+                usm_unknown_security_name_counter +=1
+            
+            else:
+                raise e
