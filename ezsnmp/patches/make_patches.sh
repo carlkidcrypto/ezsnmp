0716a42f0ccba525cf7b815b12ee675295a8ac9b
Carlos <dose.lucky.sake@cloak.id>
2024-10-02 09:56:38 -0700

    patches
    - updating patch files

---
+++ ezsnmp/patches/make_patches.sh
@@ -0,0 +1,5 @@
+diff -Naurw ~/Downloads/net-snmp-master/apps/snmpwalk.c ../src/snmpwalk.cpp > snmpwalk.patch
+diff -Naurw ~/Downloads/net-snmp-master/apps/snmpget.c ../src/snmpget.cpp > snmpget.patch
+diff -Naurw ~/Downloads/net-snmp-master/apps/snmpset.c ../src/snmpset.cpp > snmpset.patch
+diff -Naurw ~/Downloads/net-snmp-master/apps/snmpbulkwalk.c ../src/snmpbulkwalk.cpp > snmpbulkwalk.patch
+diff -Naurw ~/Downloads/net-snmp-master/apps/snmpbulkget.c ../src/snmpbulkget.cpp > snmpbulkget.patch
