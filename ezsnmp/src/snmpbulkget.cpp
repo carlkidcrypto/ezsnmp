6b3a1629693524d3d492565032fd45096fbdcd15
Carlos <dose.lucky.sake@cloak.id>
2024-11-16 20:28:56 -0800

    Clang Format
    - clang formatter

--- ezsnmp/src/snmpbulkget.cpp
+++ ezsnmp/src/snmpbulkget.cpp
@@ -130,8 +130,8 @@ void snmpbulkget_optProc(int argc, char *const *argv, int opt) {
 
 std::vector<Result> snmpbulkget(std::vector<std::string> const &args) {
    /* completely disable logging otherwise it will default to stderr */
-    netsnmp_register_loghandler(NETSNMP_LOGHANDLER_NONE, 0);
-    
+   netsnmp_register_loghandler(NETSNMP_LOGHANDLER_NONE, 0);
+
    thread_local int argc;
    thread_local std::unique_ptr<char *[]> argv = create_argv(args, argc);
 
--- ezsnmp/src/snmptrap.cpp
+++ ezsnmp/src/snmptrap.cpp
@@ -115,8 +115,8 @@ void snmptrap_optProc(int argc, char *const *argv, int opt) {
 
 int snmptrap(std::vector<std::string> const &args) {
    /* completely disable logging otherwise it will default to stderr */
-    netsnmp_register_loghandler(NETSNMP_LOGHANDLER_NONE, 0);
-    
+   netsnmp_register_loghandler(NETSNMP_LOGHANDLER_NONE, 0);
+
    thread_local int argc;
    thread_local std::unique_ptr<char *[]> argv = create_argv(args, argc);
 
