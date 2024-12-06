35d6fd9f795edb7f8f126a4453cd61bb20e030d0
Carlos <dose.lucky.sake@cloak.id>
2024-12-06 07:54:43 -0800

    Workflow
    - optimizing workflows
    - bumping the sleep time for that walk( test that timeouts on MacOS.

--- tests/test_netsnmp.py
+++ tests/test_netsnmp.py
@@ -195,7 +195,7 @@ def test_snmpwalk(netsnmp_args):
         assert res[5].value == "my original location"
 
     # Space out our tests to avoid overwhelming the snmpd server with traffic.
-    sleep(uniform(0.5, 1.0))
+    sleep(uniform(0.8, 1.3))
 
 
 def test_snmp_walk_res(netsnmp_args):
