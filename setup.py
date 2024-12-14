2eb37f5835121feaf29a85599a9e7222f1409691
Carlos <dose.lucky.sake@cloak.id>
2024-12-14 12:59:50 -0800

    Update setup.py
    - remove RelinkLibraries, don't think it is needed anymore... ???>?>?

--- setup.py
+++ setup.py
@@ -3,12 +3,7 @@
 from subprocess import check_output, CalledProcessError
 from sys import argv, platform
 from shlex import split as s_split
-
-import sysconfig
 from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext as BuildCommand
-import setuptools.command.build as build
-from setuptools import dist
 from re import search
 
 
@@ -237,59 +232,6 @@ print(f"macports_openssl_version: {macports_openssl_version}")
 print(f"libs: {libs}")
 print(f"libdirs: {libdirs}")
 print(f"incdirs: {incdirs}")
-
-
-class RelinkLibraries(BuildCommand):
-    """Fix dylib path for macOS
-
-    Depending on system configuration and Brew setup, interface.so may get linked to
-    the wrong dylib file (prioritizing the system's 5.6.2.1 over brew's 5.8+). This
-    will change the path if net-snmp is installed via `brew`.
-
-    Non-brew installations and non-macOS systems will not be affected.
-    """
-
-    def run(self):
-        BuildCommand.run(self)
-        if platform == "darwin":  # Newer Net-SNMP dylib may not be linked to properly
-            try:
-                brew = check_output(
-                    "brew list net-snmp 2>/dev/null", shell=True
-                ).decode()
-            except CalledProcessError:
-                return
-            lib_dir = list(filter(lambda l: "lib/libnetsnmp.dylib" in l, lines))[0]
-            b = build.build(dist.Distribution())  # Dynamically determine build path
-            b.finalize_options()
-            ext = sysconfig.get_config_var("EXT_SUFFIX") or ".so"  # None for Python 2
-            linked = (
-                check_output(
-                    (
-                        "otool -L {0}/ezsnmp/interface{1} | "
-                        r"egrep 'libnetsnmp\.' | "
-                        "tr -s '\t' ' ' | "
-                        "cut -d' ' -f2"
-                    ).format(b.build_platlib, ext),
-                    shell=True,
-                )
-                .decode()
-                .strip()
-            )
-
-            if linked:
-                # install_name_tool -change /opt/homebrew/opt/net-snmp/lib/libnetsnmp.40.dylib /opt/homebrew/Cellar/net-snmp/5.9.4/lib/libnetsnmp.dylib build/lib.macosx-10.9-universal2-cpython-39/ezsnmp/interface.cpython-39-darwin.so
-                cmd_to_run = (
-                    "install_name_tool -change {0} {1} {2}/ezsnmp/interface{3}".format(
-                        linked, lib_dir, b.build_platlib, ext
-                    )
-                )
-
-                print("cmd_to_run: ", cmd_to_run)
-                _ = check_output(
-                    cmd_to_run,
-                    shell=True,
-                )
-
 
 setup(
     ext_modules=[
