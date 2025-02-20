798c701986d734549fe70d65c1dc6327a70f9662
Carlos <dose.lucky.sake@cloak.id>
2025-02-19 22:22:45 -0800

    289 bug session parameters are missing in v200b1 (#298)
    * Update sessionbase_cpp.rst
    
    - adding documentation on mappings from netsnmp apps to SessionBase.cpp constructor.
    
    * Documentation
    
    - this commit focuses on updating missing doc strings inside session.py. Updates in sessionbase.rst explain mapping of values from netsnmp apps to SessionBase.
    
    * Update netsnmp.py
    
    - updating the netsnmp doc string with examples as well.
    
    * netsnmp
    
    - flushed out better doc strings for the netsnmp py file
    
    * More Parameter Support
    
    - adding support for these parameters too:
    
        {"load_mibs", "-m"},
        {"mib_directories", "-M"},
        {"print_enums_numerically", "-Oe"},
        {"print_full_oids", "-Of"},
        {"print_oids_numerically", "-On"},
    
    * Migration Guide
    
    - adding migration guide
    - adding in new parameters to Session.py
    
    * New Parameters
    
    - reworked the new parameters that are being added.
                    load_mibs,
                    mib_directories,
                    print_enums_numerically,
                    print_full_oids,
                    print_oids_numerically,
    
    * Swig, Clang, Black
    
    - ran black
    - ran clang format
    - ran swig again, but this time using 4.3.0
    
    * print_enums_numerically, print_full_oids, print_oids_numerically
    
    - adding testing for the 3 new -O options in the session class.
    
    * UTs
    
    - working on adding UTs for print_enums_numerically, print_full_oids, and print_oids_numerically. Seeing strange failing behavior in UTs.
    
    * UTs
    
    Results (127.99s (0:02:07)):
         196 passed
          10 failed
    
    Still working on those 10 failing ones.
    
    Updated documentation really quick.
    
    * More UT
    
    - adding in more UTs to test the new bool options, still having odd failures. Some pass if ran individually and fail when ran as a suite. This can be a bug in the sessionbase code perhaps.
    
    * Sphinx & Doxygen
    
    - ran sphinx and doxygen before pr.
    
    * Update test_session.py
    
    - Black.

--- tests/test_auth_priv_v3.py
+++ tests/test_auth_priv_v3.py
@@ -1,6 +1,4 @@
 from ezsnmp import Session
-from time import sleep
-from random import uniform
 
 
 def test_v3_authentication_md5_privacy_des(sess_v3_md5_des):
@@ -20,8 +18,6 @@ def test_v3_authentication_md5_privacy_des(sess_v3_md5_des):
 
 
 def test_v3_authentication_md5_privacy_aes(sess_v3_md5_aes):
-    # Space out our tests to avoid overwhelming the snmpd server with traffic.
-    sleep(uniform(0.1, 0.25))
     s = Session(**sess_v3_md5_aes)
 
     assert s.auth_passphrase == "auth_pass"
@@ -38,8 +34,6 @@ def test_v3_authentication_md5_privacy_aes(sess_v3_md5_aes):
 
 
 def test_v3_authentication_sha_privacy_aes(sess_v3_sha_aes):
-    # Space out our tests to avoid overwhelming the snmpd server with traffic.
-    sleep(uniform(0.1, 0.25))
     s = Session(**sess_v3_sha_aes)
 
     assert s.auth_passphrase == "auth_second"
@@ -56,8 +50,6 @@ def test_v3_authentication_sha_privacy_aes(sess_v3_sha_aes):
 
 
 def test_v3_authentication_sha_no_priv(sess_v3_sha_no_priv):
-    # Space out our tests to avoid overwhelming the snmpd server with traffic.
-    sleep(uniform(0.1, 0.25))
     s = Session(**sess_v3_sha_no_priv)
 
     assert s.auth_passphrase == "auth_second"
@@ -74,8 +66,6 @@ def test_v3_authentication_sha_no_priv(sess_v3_sha_no_priv):
 
 
 def test_v3_authentication_md5_no_priv(sess_v3_md5_no_priv):
-    # Space out our tests to avoid overwhelming the snmpd server with traffic.
-    sleep(uniform(0.1, 0.25))
     s = Session(**sess_v3_md5_no_priv)
 
     assert s.auth_passphrase == "auth_pass"
