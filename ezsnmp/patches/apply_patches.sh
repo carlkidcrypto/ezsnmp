ae260480029b60e61b67b9b98a9c6df2ef10f94b
Carlos <dose.lucky.sake@cloak.id>
2025-03-23 18:32:19 -0600

    V2.0.0b3
    - bump to the next beta release.
    - clean up the make patches file

--- ezsnmp/patches/apply_patches.sh
+++ ezsnmp/patches/apply_patches.sh
@@ -10,7 +10,7 @@ for patch in "$PATCH_DIR"/*.patch; do
     patched_files=$(patch -p1 --dry-run < "$patch" | grep "patching file" | awk '{print $3}' | tr -d "'")
     patch -p1 < "$patch"
     for file in $patched_files; do
-        new_file_name="$(basename "$file" .c).cpp"
+        new_file_name="$(basename "$file" .c)"
         echo "Moving and renaming patched file $file to $PATCH_DIR/$new_file_name..."
         rm -f "$PATCH_DIR/$new_file_name"
         cp ./"$file" "$PATCH_DIR/$new_file_name"
@@ -28,3 +28,5 @@ for new_file in "${NEW_FILES[@]}"; do
     echo "Moving $new_file to $SRC_DIR..."
     mv "$PATCH_DIR/$new_file" "$SRC_DIR/"
 done
+
+rm -drf ./src
--- sphinx_docs_build/source/conf.py
+++ sphinx_docs_build/source/conf.py
@@ -24,7 +24,7 @@ copyright = "2024-%Y, carlkidcrypto"
 author = "carlkidcrypto"
 
 # The full version, including alpha/beta/rc tags
-release = "V2.0.0b2"
+release = "v2.0.0b3"
 
 
 # -- General configuration ---------------------------------------------------
