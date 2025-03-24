9ff225e05ea70091b2d4df1d3bd8c4822521d790
Carlos <dose.lucky.sake@cloak.id>
2025-03-24 00:06:11 -0600

    cleanup
    - last few changes before merge

--- ezsnmp/patches/make_patches.sh
+++ ezsnmp/patches/make_patches.sh
@@ -1,7 +1,7 @@
-diff -Naurw ./net-snmp-master/apps/snmpwalk.c ../src/snmpwalk.cpp > snmpwalk.patch
-diff -Naurw ./net-snmp-master/apps/snmpget.c ../src/snmpget.cpp > snmpget.patch
-diff -Naurw ./net-snmp-master/apps/snmpset.c ../src/snmpset.cpp > snmpset.patch
-diff -Naurw ./net-snmp-master/apps/snmptrap.c ../src/snmptrap.cpp > snmptrap.patch
-diff -Naurw ./net-snmp-master/apps/snmpbulkwalk.c ../src/snmpbulkwalk.cpp > snmpbulkwalk.patch
-diff -Naurw ./net-snmp-master/apps/snmpbulkget.c ../src/snmpbulkget.cpp > snmpbulkget.patch
-diff -Naurw ./net-snmp-master/apps/snmpgetnext.c ../src/snmpgetnext.cpp > snmpgetnext.patch
+diff -aurw ./net-snmp/apps/snmpwalk.c ../src/snmpwalk.cpp > snmpwalk.patch
+diff -aurw ./net-snmp/apps/snmpget.c ../src/snmpget.cpp > snmpget.patch
+diff -aurw ./net-snmp/apps/snmpset.c ../src/snmpset.cpp > snmpset.patch
+diff -aurw ./net-snmp/apps/snmptrap.c ../src/snmptrap.cpp > snmptrap.patch
+diff -aurw ./net-snmp/apps/snmpbulkwalk.c ../src/snmpbulkwalk.cpp > snmpbulkwalk.patch
+diff -aurw ./net-snmp/apps/snmpbulkget.c ../src/snmpbulkget.cpp > snmpbulkget.patch
+diff -aurw ./net-snmp/apps/snmpgetnext.c ../src/snmpgetnext.cpp > snmpgetnext.patch
