5277cb2683b0bc84d13e1e6128213fc9f35adb89
Carlos <dose.lucky.sake@cloak.id>
2025-04-27 10:20:13 -0700

    Valgrind
    - this time maybe valgrind will work.

--- python_tests/test_datatypes.py
+++ python_tests/test_datatypes.py
@@ -6,6 +6,7 @@ from ezsnmp import Session
 
 faulthandler.enable()
 
+
 @pytest.mark.parametrize("version", ["1", "2c", "3", 1, 2, 3])
 def test_string_values_not_enclosed_in_quotes(version):
     """
@@ -30,12 +31,17 @@ def test_string_values_not_enclosed_in_quotes(version):
             version="2c",
         )
 
-    result = session.get([".1.3.6.1.2.1.1.1.0", ])  # System
+    result = session.get(
+        [
+            ".1.3.6.1.2.1.1.1.0",
+        ]
+    )  # System
     assert len(result) > 0, "No results returned from SNMP get operation"
     value = result[0].value
 
     # Ensure the value is a string and not enclosed in quotes
     print(f"Value: {value}")
     assert isinstance(value, str), "Returned value is not a string"
-    assert not (value.startswith('"') and value.endswith('"')), "String value is enclosed in quotes"
-
+    assert not (
+        value.startswith('"') and value.endswith('"')
+    ), "String value is enclosed in quotes"
